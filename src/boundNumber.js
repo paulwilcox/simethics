e1d675da70d2c340558c3b2886b0314821c34ac4
paulwilcox <t78t78@gmail.com>
2021-08-08 15:33:46 -0700

    boundNumber.js: introduce dep/ext rates. Get rid of constructor conveniences.

--- src/boundNumber.js
+++ src/boundNumber.js
@@ -1,13 +1,23 @@
 export default class boundNumber {
 
-    constructor(num, lower, upper) {
+    constructor(num) {
+        
         this.number = num;
+        
+        // the abolute bounds 
         this.lower = -Infinity;
         this.upper = Infinity;
-        if (this.lower !== undefined)
-            this.setLower(lower);
-        if(this.upper !== undefined)
-            this.setUpper(upper);
+
+        // the bounds per unit of time
+        this.extractRate = Infinity;
+        this.depositRate = Infinity;
+
+        // aliases
+        this.l = this.setLower();
+        this.u = this.setUpper();
+        this.er = this.setExtractRate();
+        this.dr = this.setDepositRate();
+
     }
 
     setLower(val) {
@@ -24,33 +34,56 @@ export default class boundNumber {
         return this;
     }
 
-    // Request a value to extract.  
-    // If that value would put you below the constraint,
-    // then only extract up to that constraint.
-    extract(val) {
-        if (val === undefined)
-            val = this.number;
-        if (this.number - val <= this.lower)
-            val = this.number - this.lower;
-        this.number -+ this.val;
-        return val;       
+    setExtractRate(val) {
+        this.extractRate = val;
+        return this;
     }
 
-    // Request a value to deposit.
-    // If that value would put you above the constraint,
-    // then only deposit up to that contraint.
-    // Return the excess above and beyond that constraint.
-    deposit(val) {
+    setDepositRate(val) {
+        this.depositRate = val;
+        return this;
+    }
+
+    extract(val = Infinity, time) {
+
+        if (time === undefined)
+            throw `The 'time' parameter is required for an extraction.`;
+        
+        // only allow extraction according to the permitted rate
+        if (val > this.extractRate * time)
+            val = this.extractRate * time;
+        
+        // only allow extraction up to the permitted absolute minimum 
+        if (this.number - val <= this.lower)
+            val = this.number - this.lower;
+
+        this.number -+ this.val;
+        return val;
+
+    }
+
+    deposit(val = Infinity, time) {
+
+        if (time === undefined)
+            throw `The 'time' parameter is required for a deposit.`;
+        
         let excess = 0;
-        if (val === undefined)
-            val = this.number;
-        if (this.number + val >= this.upper) {
-            let _val = this.upper - this.number;
-            excess = val - _val;
+
+        if (val > this.depositRate * time) {
+            _val = this.depositRate * time;
+            excess += val - _val;
             val = _val;
         }
+
+        if (this.number + val >= this.upper) {
+            let _val = this.upper - this.number;
+            excess += val - _val;
+            val = _val;
+        }
+
         this.number += val;
         return excess;
+
     }
 
 }
