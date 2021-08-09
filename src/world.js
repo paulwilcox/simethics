41a631e78e31f17e8a250188a7de2479c3559f4b
paulwilcox <t78t78@gmail.com>
2021-08-08 17:11:13 -0700

    world.js: going back to explicit 'catchers'. mental stoppoint with comments.

--- src/world.js
+++ src/world.js
@@ -17,37 +17,53 @@ let n = bnm(time);
 
 world.push(...[
 
-    { concept: 'mine', keys: ['metal','rock'] },
-    { concept: 'person', keys: ['happiness'] },
-    { concept: 'battery', keys: ['energy'] },
-
-    { metal: n(50).l(0), rock: n(100).l(0) },
-    { metal: n(25).l(0), rock: n(50).l(0) },
+    { metal: n(50).l(0).er(2), rock: n(100).l(0) },
+    { metal: n(25).l(0).er(2), rock: n(50).l(0) },
     
     { 
         name: 'Ariceli', 
         happiness: n(0).l(0).u(100), 
-        energy: n(20).l(0).u(100) 
+        energy: n(20).l(0).u(100).er(5) 
     },
     { 
         name: 'Aaron', 
         happiness: n(0).l(0).u(100), 
-        energy: n(35).l(0).u(100) 
+        energy: n(35).l(0).u(100).er(5) 
     },
 
-    // A computer requires 2 parts metal and 1 part energy
-    // This serves to demonstrate 'conversion'
-    function makeComputers (mine,battery) {
+    // world will be recursed.  
+    // input/output objects will be identified
+    // inputs and outputs will 'catch' other world objects
+    // the objects will be processed according to the throughput
+    { 
+        name: 'industry',
+        inputs: {
+            mine: ['metal','rock'],
+            battery: 'energy'
+        },
+        outputs: {
+            person: 'happiness'
+        },
+        throughput: (mine,battery,person) => {
 
-        // extract according to the limits of the core equation below
-        let metal = mine.metal.extract(battery.energy * 2);
-        let energy = battery.energy.extract(mine.metal / 2);
+            let possibleImprovement = person.happiness.upper - person.happiness.value;
 
-        // The core equation.
-        world.push({ computers: (metal * 2) + energy });
+            // max metal due to improvement cap: metal = (posi - energy) / 2
+            // max energy due to improvement cap: energy = posi - (metal * 2)
 
+            // extract according to the limits of the core equation below
+            let metal = mine.metal.extract(battery.energy * 2);
+            let energy = battery.energy.extract(mine.metal / 2);
+
+            // The core equation.
+            person.happiness.deposit((metal * 2) + energy);
+
+        }
     }
 
 ]);
 
 
+for(let member of world) {
+
+}
